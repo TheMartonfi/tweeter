96882ac1da41a0e6981ebef29e9b6e8e68366574
TheMartonfi <carlmartonfi@hotmail.com>
2020-09-09 21:28:48 +0000

    Made load tweet function that makes an AJAX get request to get tweets and also changed the AJAX post request to load tweets with the submit even handler

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,32 +1,6 @@
 $(() => {
 
-  const tweets = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
   const createTweetElement = (tweetData) => {
-
     const $tweet = $(`<article class="tweet"></article>`);
     const $header = $(`<header></header>`);
     const $div = $('<div></div>');
@@ -48,8 +22,29 @@ $(() => {
 
   const renderTweets = (tweets) => {
     for (const tweet of tweets) {
-      $('#tweets-container').append(createTweetElement(tweet));
+      $('#tweets-container').prepend(createTweetElement(tweet));
     }
   };
-  renderTweets(tweets);
+
+  const loadTweets = () => {
+    $.get('/tweets')
+    .then((response) => {
+      $('#tweets-container').empty();
+      renderTweets(response);
+    });
+  };
+
+  $('form').submit(function(event) {
+    event.preventDefault();
+
+    const $text = $(this).children('textarea');
+    const serializedText = $text.serialize();
+
+    $.post('/tweets', serializedText)
+    .then(() => {
+      loadTweets();
+    });
+  });
+
+  loadTweets();
 });
